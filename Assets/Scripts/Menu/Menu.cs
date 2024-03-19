3f873914628247365bb29e234e394a0b4a4db19c
albertodt <113683638+abertodt@users.noreply.github.com>
2024-03-19 12:21:03 +0100

    Alberto

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject Team;
+
     public void ExitGame()
     {
         Debug.Log("salgo");
@@ -15,5 +18,17 @@ public class Menu : MonoBehaviour
         (string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadTeam()
+    {
+        if(!Team.activeSelf) 
+        {
+            Team.SetActive(true);
+        }
+        else
+        {
+            Team.SetActive(false);
+        }
     }
 }
